86425a06ce3ced5b08a9db19752770d0a128ebc9
Bug5 <lebogangmasia101@outlook.com>
2022-06-22 08:46:49 +0200

    Merging HashTable with Tokens

--- Rython/VM/include/token.hpp
+++ Rython/VM/include/token.hpp
@@ -1,56 +1,35 @@
 #ifndef RYTHON_TOKEN_HPP
 #define RYTHON_TOKEN_HPP
 #include <cstring>
-#include <sstream>
-#include <vector>
-#include <regex>
-#include "../include/table.hpp"
-#include "util.hpp"
+
+typedef struct Token Token;
+typedef struct HashTableItem HashTableItem;
+typedef struct HashTable HashTable;
 
 struct Token {
     char* type;
     char* value;
 };
 
-
-unsigned int HashFunction(char* str);
-
-typedef struct HashTableItem HashTableItem;
-typedef struct HashTable HashTable;
-
-// define the hash table item
 struct HashTableItem {
-    char* key;
-    char* value;
+    Token* token;
 };
 
-// define the hash table
 struct HashTable {
-    // contains an array of pointers to items
+    // an array of pointers to HashTableItem
     HashTableItem** items;
-     int size; // max size of the table
-     int count; // how many items are in the HashTable
+    // TODO: DO NOT HARDCODE THE SIZE
+    int size; // the max size of the HashTable
+    int count; // How many items are currently on the list;
 };
 
+// create the Token, HashTableItem and HashTable
+[[maybe_unused]] int HashFunction(char* type, char* value);
 Token* createToken(char* type, char* value);
-HashTable* Tokenize(char* lineBuf);
-
-
-// create the hashtable and it's items
-HashTableItem* createItem(Token* Token);
+HashTableItem* createItem(Token* token);
 HashTable* createTable(int size);
 
-// memory management
-void freeItem(HashTableItem* item);
-void freeTable(HashTable* table);
-
-// hash table algorithms
-void HashTableInsert(HashTable* table, Token* token);
-char* HashTableSearch(HashTable* table, char* key);
-void HashTableDelete(HashTable* table, char* key);
-
-// testing
-void printSearch(HashTable* table, char* key);
-void printTable(HashTable* table);
+// HashTable algorithms
+void HashTableInsert(HashTable* table, char* value, char* type);
 
 #endif // RYTHON_TOKEN_HPP
